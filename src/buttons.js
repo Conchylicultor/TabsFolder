641888895fa5baa0512fc312e721726f3b36af62
uranium78 <bruno78786767@gmail.com>
2016-11-27 17:53:57 -0500

    Add files via upload

--- src/buttons.js
+++ src/buttons.js
@@ -1,62 +1,61 @@
-$( document ).ready(function() {
-    
+$(document).ready(function() {
+
   $("#create_group").click(function() {
     createGroup();
     // Sauvegarde
     saveGroup();
   });
 
-  $("#list_groups").on( "click", ".group_remove", function() {
+  $("#list_groups").on("click", ".group_remove", function() {
     var group = getGroup(getGroupId($(this)));
-    
-    if(group.id == groupActif.id)
-    {
+
+    if (group.id == groupActif.id) {
       // On defini le prochain groupe comme etant actif
-      var next_group = list_groups[0];// On prend le premier groupe que l'on trouve
-      if(next_group.id == groupActif.id)// Si c'est le meme
+      var next_group = list_groups[0]; // On prend le premier groupe que l'on trouve
+      if (next_group.id == groupActif.id) // Si c'est le meme
       {
-	// Il existe un autre groupe
-	if(list_groups.length > 1)
-	{
-	  next_group = list_groups[1];
-	}
-	// Ou pas
-	else
-	{
-	  next_group = createGroup();
-	}
+        // Il existe un autre groupe
+        if (list_groups.length > 1) {
+          next_group = list_groups[1];
+        }
+        // Ou pas
+        else {
+          next_group = createGroup();
+        }
       }
-      
+
       //ferme les onglets du groupe courant et ouvrent ceux du prochain
       setGroupActif(next_group);
-      
+
       // Sauvegarde
       saveGroup();
+      
     }
-    
+
     // Modification de l'affichage
     $("#group_id_" + group.id.toString()).remove();
-    
+
     // Suppression du groupe
     list_groups.splice(list_groups.indexOf(group), 1);
-    
+
     // Sauvegarde
     saveGroup();
   });
 
-  $("#list_groups").on( "click", ".group_set_actif", function() {
+  $("#list_groups").on("click", ".group_set_actif", function() {
     setGroupActif(getGroup(getGroupId($(this))));
-    
+
     // Sauvegarde
     saveGroup();
+    close();
   });
 
-  $("#list_groups").on( "blur", ".group_name", function() {
+  $("#list_groups").on("blur", ".group_name", function() {
     group = getGroup(getGroupId($(this)));
     group.name = $(this).val();
-    
+
     // Sauvegarde
     saveGroup();
-  }); 
+  });
 
 });
